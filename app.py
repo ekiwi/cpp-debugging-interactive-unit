81b56de49dc2d1cc88062dc894763973b184f33f
Kevin Läufer <laeufer@cs.berkeley.edu>
2019-04-07 18:21:56 -0700

    server: serve simple html

---
+++ app.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright 2019 Kevin Laeufer <laeufer@cs.berkeley.edu>
+
+from server import *
+
+p1 = """
+int main(int argc, const char* argv[]) {
+    int buffer[10];
+    itn x;
+    for(int ii = 1; ii <= 10; ii += 1) {
+        buffer[ii] = ii;
+    }
+    std::cout << buffer[10] << std::endl;
+}
+"""
+
+mk1 = """
+program: main.c
+	clang main.c -o program
+"""
+
+p2 = """
+struct Counter {
+    int* count;
+    Counter() {}
+    void increment() { *count += 1; }
+    int get() { return *count; }
+};
+int main(int argc, const char* argv[]) {
+    Counter count;
+    count.increment()
+    count.increment()
+    std::cout << count.get() << std::endl;
+}
+"""
+
+p3 = """
+struct particle_t { int x; int y; };
+void reset_particle(particle_t* part_ptr) {
+    particle_t part = *part_ptr;
+    part = {0, 0};
+}
+int main(int argc, const char* argv[]) {
+particle_t part = {10, 10};    
+    std::cout << “x=” << part.x << “; y=” << part.y << std::endl;
+reset_particle(&part);
+    std::cout << “x=” << part.x << “; y=” << part.y << std::endl;
+}
+"""
+
+p4 = """
+struct particle_t { int x; int y; };
+particle_t* make_particle() {
+    particle_t part = { -10, 10 };
+    return &part;
+}
+int main(int argc, const char* argv[]) {
+particle_t* part = make_particle();
+    std::cout << “x=” << part->x << “; y=” << part->y << std::endl;
+}
+"""
+
+app_dir = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+
+if __name__ == '__main__':
+	address = ("localhost", 12345)
+	student_dir = 'students'
+	code_mirror = 'codemirror-5.45.0'
+	lib_dirs = [os.path.join('ext', code_mirror, dd) for dd in ['lib', 'mode/clike']]
+	app = App([
+		Part(1, name="Program 1", program=p1, makefile=mk1, steps=[
+			QuestionStep(2, "What should the program do?",
+						 "Please take a moment to read through the source code of the program provided below. What do you think is the program intended to do? Put your thoughts into the text box below.",
+						 "What do you think the program output will be?"),
+			QuestionStep(3, "What are potential bugs?", "", ""),
+			QuestionStep(4, "What would you do to debug?", "", ""),
+			RunStep(5, "Run the program", ""),
+			ModifyStep(6, "Try Debug Technique 1", ""),
+			ModifyStep(7, "Fix Program", "")
+		])
+
+
+	], student_dir=student_dir)
+	serv = Server(address=address, app=app, student_dir=student_dir, lib_dirs=lib_dirs, app_dir=app_dir)
+	serv.serve_forever()
--- server.py
+++ server.py
@@ -3,3 +3,222 @@
 
 # Copyright 2019 Kevin Laeufer <laeufer@cs.berkeley.edu>
 
+import json, os, sys
+import http.server
+from urllib.parse import urlparse
+from typing import List
+from functools import reduce, total_ordering
+import operator
+
+def assert_uids(items):
+	uids = {s.uid for s in items}
+	assert len(uids) == len(items), "non unique id!"
+
+
+class Step:
+	def __init__(self, uid: int, name: str, instructions: str):
+		self.uid = uid
+		self.name = name
+		self.instructions = instructions
+
+class QuestionStep(Step):
+	def __init__(self, uid: int, name: str, instructions: str, question: str):
+		super().__init__(uid=uid, name=name, instructions=instructions)
+		self.question = question
+		self.answers = []
+
+class RunStep(Step):
+	def __init__(self, uid: int, name: str, instructions: str):
+		super().__init__(uid=uid, name=name, instructions=instructions)
+
+class ModifyStep(Step):
+	def __init__(self, uid: int, name: str, instructions: str):
+		super().__init__(uid=uid, name=name, instructions=instructions)
+
+
+class Part:
+	def __init__(self, uid: int, name: str, program: str, makefile: str, steps: List[Step]):
+		assert_uids(steps)
+		self.uid = uid
+		self.name = name
+		self.program = program
+		self.makefile = makefile
+
+		self.steps = {s.uid: s for s in steps}
+
+class Student:
+	def __init__(self, uid, progress, answers):
+		self.uid = uid
+		self.progress = progress
+		self.answers = answers
+	@staticmethod
+	def load(filename: str, start):
+		assert filename.endswith('.json')
+		expected_uid = os.path.basename(filename)[:-len('.json')]
+		with open(filename) as ff:
+			dd = json.load(ff)
+		assert dd['uid'] == expected_uid, f"{dd['uid']} != {expected_uid}"
+		if dd['progress'] is None: dd['progress'] = start
+		return Student(**dd)
+	def save(self, student_dir: str):
+		assert os.path.isdir(student_dir)
+		filename = os.path.join(student_dir, self.uid + ".json")
+		dd = {'uid': self.uid, 'progress': self.progress, 'answers': self.answers}
+		with open(filename, 'w') as ff:
+			json.dump(dd, ff)
+
+class Error:
+	def __init__(self, msg):
+		self.msg = msg
+	def __str__(self):
+		return f"Error({self.msg})"
+class Success:
+	def __init__(self, dat):
+		self.dat = dat
+	def __str__(self):
+		return f"Success({self.dat})"
+
+def is_error(e): return isinstance(e, Error)
+
+@total_ordering
+class PathId:
+	def __init__(self, part, step, app):
+		assert isinstance(part, int)
+		assert isinstance(step, int)
+		assert isinstance(app, App)
+		self.part = part
+		self.step = step
+		self.progress = app.uid_progress[(self.part, self.step)]
+	def __eq__(self, other):
+		assert isinstance(other, PathId)
+		return self.progress == other.progress
+	def __ne__(self, other):
+		return not(self == other)
+	def __lt__(self, other):
+		assert isinstance(other, PathId)
+		return self.progress < other.progress
+
+class App:
+	def __init__(self, parts: List[Part], student_dir):
+		assert_uids(parts)
+		self.parts = {p.uid: p for p in parts}
+		self.students = {}
+		# make uids comparable
+		uids = reduce(operator.add, ([(p.uid, s.uid) for s in p.steps.values()] for p in parts))
+		self.start = uids[0]
+		self.uid_progress = {uid: ii for ii, uid in enumerate(uids)}
+		self.app_html = None
+		# command list
+		self.cmds = {'load': self.load_step, 'run': self.run, 'answer': self.answer }
+		# student directory
+		assert os.path.isdir(student_dir)
+		self.student_dir = student_dir
+
+	def load_assets(self, app_html):
+		assert self.app_html is None
+		with open(app_html) as ff:
+			self.app_html = ff.read()
+
+	def load_students(self, student_dir):
+		assert os.path.isdir(student_dir)
+		assert len(self.students) == 0, "cannot load students twice!"
+		_, _, files = next(os.walk(student_dir))
+		for filename in files:
+			if filename.endswith('.json'):
+				stud = Student.load(os.path.join(student_dir, filename), self.start)
+				self.students[stud.uid] = stud
+				for (part, step), answer in stud.answers.items():
+					self.parts[part].steps[step].answers.append((stud.uid, answer))
+
+	def parse_student_path(self, student_id, path):
+		if len(path) == 0:
+			path = self.start
+		if student_id not in self.students: return Error("unknown student")
+		if len(path) != 2: return Error("path needs to be (part, step)")
+		try:
+			part = int(path[0])
+			step = int(path[1])
+		except ValueError:
+			return Error(f"invalid path {path}")
+		if part not in self.parts: return Error(f"unknown part: {part}")
+		if step not in self.parts[part].steps: return Error(f"unknown step {step} for part {part}")
+		path_id = PathId(part=part, step=step, app=self)
+		student = self.students[student_id]
+		if self.uid_progress[student.progress] < path_id.progress: return Error("student is not at this step yet")
+		part = self.parts[path_id.part]
+		step = part.steps[path_id.step]
+		return Success((student, part, step))
+
+	def exec(self, cmd, student_id, path_list, content):
+		ret = self.parse_student_path(student_id, path_list)
+		if is_error(ret): return ret
+		if cmd not in self.cmds: return Error(f"unknown command: {cmd}")
+		return self.cmds[cmd](*ret.dat, content)
+
+	def load_step(self, student, part, step, _):
+		return Success(self.app_html)
+
+	def run(self, student, part, step, sources):
+		can_run = isinstance(step, RunStep) or isinstance(step, ModifyStep)
+		if not can_run: return Error("cannot run in this step")
+		if isinstance(step, RunStep):
+			# a run step supports no modifications
+			main_src = part.program
+			make_src = part.makefile
+		else:
+			main_src, make_src = sources['main'], sources['make']
+		# TODO: compile and run sources
+		return Success("{'ret': 'fail'}")
+
+	def answer(self, student, part, step, text):
+		step_id = (part.uid, step.uid)
+		student.answers[step_id] = text
+		student.save(self.student_dir)
+		step.answers.append((student.uid, text))
+		return Success("{'ret': 'success'}")
+
+class Handler(http.server.BaseHTTPRequestHandler):
+	def parse_path(self):
+		app = self.server.app
+		pp = self.path.split('/')[1:]
+		if 5 > len(pp) > 2 and pp[0] in app.students:
+			if len(pp) == 3:
+				pp += ['load']
+			student_id, p0, p1, cmd = pp
+			return app.exec(cmd, student_id, (p0, p1), 'TODO')
+		pdir = os.path.dirname(self.path)
+		for suffix, lib_dir in self.server.lib_dirs.items():
+			if pdir.endswith(suffix):
+				filename = os.path.join(lib_dir, os.path.basename(self.path))
+				with open(filename) as ff:
+					return Success(ff.read())
+		return Error(f"unknown path: {self.path}")
+
+	def do_GET(self):
+		ret = self.parse_path()
+		print(ret)
+		if is_error(ret):
+			self.send_response(404)
+			self.end_headers()
+			self.wfile.write(self.server.error_html)
+		else:
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write(ret.dat.encode('utf8'))
+
+
+
+class Server(http.server.ThreadingHTTPServer):
+	def __init__(self, address, app, student_dir, lib_dirs, app_dir):
+		assert isinstance(app, App)
+		assert os.path.isdir(app_dir)
+		assert all(os.path.isdir(os.path.join(app_dir, dd)) for dd in lib_dirs)
+		self.app = app
+		self.app.load_assets(app_html=os.path.join(app_dir, 'app.html'))
+		self.app.load_students(student_dir=os.path.join(app_dir, student_dir))
+		self.lib_dirs = {dd: os.path.join(app_dir, dd) for dd in lib_dirs}
+		with open(os.path.join(app_dir, '404.html')) as ff:
+			self.error_html = ff.read().encode('utf8')
+
+		super().__init__(address, Handler)
+
